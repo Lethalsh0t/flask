82d6575077c8d22feaea9f757b497622fa00a2ec
Lethalsh0t <opet21@gmail.com>
2023-03-29 00:22:51 -0700

    flaskcode

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ import pyodbc
 app = Flask(__name__)
 
 # Set up database connection
-cnxn = pyodbc.connect('Driver={SQLite3};Database=C:/Users/opet2/testflaskapp/chinook.db')
-cursor = cnxn.cursor()
+cnxn = pyodbc.connect('Driver={SQLite3 ODBC Driver};Database=C:/Users/opet2/testflaskapp/chinook.db')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         CustomerId = request.form['CustomerId']
         query = f"SELECT FirstName, LastName FROM customers WHERE CustomerId = '{CustomerId}'"
+        cursor = cnxn.cursor()
         cursor.execute(query)
         results = cursor.fetchall()
         return render_template('results.html', results=results)
